66288a1430899e32e68cf7519bbfb91e2e181863
Phil-C-92 <philcutner@gmail.com>
2025-08-22 11:17:31 +1000

    Putting files in vector

--- src/mmcopier.cpp
+++ src/mmcopier.cpp
@@ -3,10 +3,14 @@
 #include <string>
 #include <filesystem>
 #include <fstream>
+#include <algorithm>
+
+namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]) {
 
     int threadCount;
+    std::vector<fs::directory_entry>sortedDir;
 
     std::cout << "Number of arguments passed in: " << argc - 1 << "\n";
 
@@ -32,14 +36,23 @@ int main(int argc, char* argv[]) {
             return 0;
         }
 
-    
+    fs::path sourceDir = fs::absolute(argv[3]);
+    fs::path destDir = fs::absolute(argv[3]);
+
+    for(fs::directory_entry file : fs::directory_iterator(sourceDir))
+        {
+            sortedDir.push_back(file);
+        }
+
+    std::sort(sortedDir.begin(), sortedDir.end());
+
 
     std::cout << "Arg 1: " << argv[0] << "\n";
     std::cout << "Arg 2: " << argv[1] << "\n";
     std::cout << "Arg 3: " << argv[2] << "\n";
     std::cout << "Arg 4: " << argv[3] << "\n";
 
-    int threadCount = std::stoi(argv[1]);
+    threadCount = std::stoi(argv[1]);
 
     std::cout << "Thread count : " << threadCount << "\n";
 
