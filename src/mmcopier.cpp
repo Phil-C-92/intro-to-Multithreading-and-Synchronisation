91f93786a70253300fdceec3e32e7facae801c79
Phil-C-92 <philcutner@gmail.com>
2025-08-22 10:33:05 +1000

    test

--- src/mmcopier.cpp
+++ src/mmcopier.cpp
@@ -1,19 +1,38 @@
 #include <pthread.h>
 #include <iostream>
 #include <string>
+#include <filesystem>
+#include <fstream>
 
 int main(int argc, char* argv[]) {
+
+    int threadCount;
 
     std::cout << "Number of arguments passed in: " << argc - 1 << "\n";
 
     if(argc < 4)
         {
-            std::cout << "Not enough arguments passed in. Please try again using\n";
+            std::cout << "Incorrect arguments passed in. Please try again using\n";
             std::cout << "./mmcopier n source_dir desination_dir \n";
             std::cout << "Where n is >= 2 and <= 10\n";
 
             return 0;
         }
+
+    try 
+        {
+            threadCount = std::stoi(argv[1]);
+        }
+    catch (std::invalid_argument)
+        {
+            std::cout << "Incorrect arguments passed in. Please try again using\n";
+            std::cout << "./mmcopier n source_dir desination_dir \n";
+            std::cout << "Where n is >= 2 and <= 10\n";
+
+            return 0;
+        }
+
+    
 
     std::cout << "Arg 1: " << argv[0] << "\n";
     std::cout << "Arg 2: " << argv[1] << "\n";
